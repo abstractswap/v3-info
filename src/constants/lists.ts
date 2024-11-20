bb39a0ea65b71e5adc878cd59363214337d1d0d8
Leoni Mella <leoni.mella@gmail.com>
2024-11-20 09:43:36 -0300

    feat: add bob token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -9,6 +9,7 @@ export const UNSUPPORTED_LIST_URLS: string[] = []
 // export const BNB_LIST = 'https://raw.githubusercontent.com/plasmadlt/plasma-finance-token-list/master/bnb.json'
 export const ABSTRACT_TESTNET_LIST = 'https://api.testnets.relay.link/tokenlist?chainId=11124'
 export const ZERO_LIST = 'https://api.relay.link/tokenlist?chainId=543210'
+export const BOB_LIST = 'https://api.relay.link/tokenlist?chainId=60808'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
@@ -19,6 +20,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   // BNB_LIST,
   ABSTRACT_TESTNET_LIST,
   ZERO_LIST,
+  BOB_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
